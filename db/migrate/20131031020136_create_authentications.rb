ade40a59a8f6ba97bb10eac36a8efbd22e967d24
Igor Krougovoi <ikrougovoi@gmail.com>
2013-11-11 16:47:04 -0500

    db changes

--- db/migrate/20131031020136_create_authentications.rb
+++ db/migrate/20131031020136_create_authentications.rb
@@ -3,6 +3,8 @@ class CreateAuthentications < ActiveRecord::Migration
     create_table :authentications do |t|
       t.integer :user_id
       t.string :provider
+      t.string :token
+      t.string :oauth_secret
       t.string :uid
 
       t.timestamps
